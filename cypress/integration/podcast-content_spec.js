17e417e056d747e1d011634c4b4944c4f11f1f18
Aarthy <rthysekar1@gmail.com>
2022-03-11 01:54:56 +0530

    mock graphql response with fixtures in cypress

--- cypress/integration/podcast-content_spec.js
+++ cypress/integration/podcast-content_spec.js
@@ -1,22 +1,30 @@
 describe('Feature: Podcast contents', () => {
-    beforeEach(() => {
-        cy.visitApp();
-    })
+  beforeEach(() => {
+    cy.intercept('POST', 'https://api.staging.tigerhall.io/graphql', req => {
+      if (req.body.operationName === 'GetContentCards') {
+        req.alias = 'gqlGetContentCardsQuery';
+        req.reply({ fixture: 'GetContentCards.json' });
+      }
+    });
+    cy.visitApp();
+  });
 
-    it('Can see Search option', () => {
-        cy.findByPlaceholderText('Type any keyword').should('exist');
-    })
+  it('Can see Search option', () => {
+    cy.findByPlaceholderText('Type any keyword').should('exist');
+  });
 
-    it('Can view loading indicator on fetching podcast contents and can view unfiltered podcasts', async () => {
-        cy.findByAltText('Loading ...').should('exist');
-        await cy.findByAltText('Loading ...').should('not.exist')
-        cy.findAllByRole('article').should('have.length', 20)
-    })
+  it('Can view loading indicator on fetching podcast contents and can view unfiltered podcasts', () => {
+    cy.wait('@gqlGetContentCardsQuery').then(() => {
+      cy.findAllByRole('article').should('have.length', 8);
+    });
+  });
 
-    it('Can view podcast content filtered based on search', async () => {
-        cy.findByRole('textbox').type('hiring');
-        cy.findByAltText('Loading ...').should('exist');
-        await cy.findByAltText('Loading ...').should('not.exist')
-        cy.findAllByRole('article').should('have.length', 2)
-    })
-})
+  it('Can view podcast content filtered based on search', () => {
+    cy.wait('@gqlGetContentCardsQuery').then(() => {
+      cy.findAllByRole('article').should('have.length', 8);
+      cy.findByRole('textbox').type('hiring');
+      cy.findByAltText('Loading ...').should('exist');
+      cy.findAllByRole('article').should('have.length', 2);
+    });
+  });
+});
