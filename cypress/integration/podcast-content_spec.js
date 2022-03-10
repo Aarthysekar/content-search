67c073b7cc58f0f0c612c0c61d9bf714ea12666e
Aarthy <rthysekar1@gmail.com>
2022-03-10 21:48:23 +0530

    added test case for podcast content display and filter logic

--- cypress/integration/podcast-content_spec.js
+++ cypress/integration/podcast-content_spec.js
@@ -6,4 +6,17 @@ describe('Feature: Podcast contents', () => {
     it('Can see Search option', () => {
         cy.findByPlaceholderText('Type any keyword').should('exist');
     })
+
+    it('Can view loading indicator on fetching podcast contents and can view unfiltered podcasts', async () => {
+        cy.findByAltText('Loading ...').should('exist');
+        await cy.findByAltText('Loading ...').should('not.exist')
+        cy.findAllByRole('article').should('have.length', 20)
+    })
+
+    it('Can view podcast content filtered based on search', async () => {
+        cy.findByRole('textbox').type('hiring');
+        cy.findByAltText('Loading ...').should('exist');
+        await cy.findByAltText('Loading ...').should('not.exist')
+        cy.findAllByRole('article').should('have.length', 2)
+    })
 })
