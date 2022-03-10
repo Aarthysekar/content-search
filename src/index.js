b0b1b67bb4cc428bba9dc3d4630940a2809f9eee
Aarthy <rthysekar1@gmail.com>
2022-03-10 15:06:19 +0530

    Added config for Absolute imports, deleted boiler plate and added placeholder for app content

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
+import * as serviceWorker from 'serviceWorker';
 
 ReactDOM.render(
   <StrictMode>
